8b392871d65693f14d11e9e844114c116e7b2a9b
Nicolas Lenz <nicolas@eisfunke.com>
2019-04-10 14:39:21 +0200

    Update uwu-clean-bot.py

--- uwu-clean-bot.py
+++ uwu-clean-bot.py
@@ -15,6 +15,6 @@ def delete(bot, update):
 token = open(TOKEN_FILE, "r").read().rstrip()
 updater = Updater(token=token)
 
-updater.dispatcher.add_handler(RegexHandler('\buwu\b|\bUWU\b|\buWu\b|\bUwU\b|\bUwu\b|\buwU\b', delete))
+updater.dispatcher.add_handler(RegexHandler('.*\b[uU][wW][uU]\b.*', delete))
 
 updater.start_polling()

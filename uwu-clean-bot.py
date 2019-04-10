8211a5590876bd987ee8db0004c30fe0ad666404
Nicolas Lenz <nicolas@eisfunke.com>
2019-04-10 14:49:39 +0200

    Update uwu-clean-bot.py

--- uwu-clean-bot.py
+++ uwu-clean-bot.py
@@ -8,7 +8,7 @@ import time
 TOKEN_FILE = "token"
 
 def delete(bot, update):
-    time.sleep(10)
+    time.sleep(2)
     bot.deleteMessage(chat_id=update.message.chat.id, message_id=update.message.message_id)
     bot.sendMessage(chat_id=update.message.chat.id, text='An illegal uwu has been deleted here.')
 
